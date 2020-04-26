3c2188a6172ac3b67cc7bc1b5409e908bbc71659
unknown <mje349@nyu.edu>
2020-04-26 03:09:49 -0700

    added custom icon and changed target color

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-alert('Hello')
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendresponse)
+{
+    console.log(message.txt);
+    let input = document.getElementsByTagName("input");
+    for (elt of input)
+    {
+        elt.style['background-color'] = '#83142c';
+    }
+}
